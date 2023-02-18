ee1e24eb168fd7d9548830a1f71fd76cff4fbe67
1nTh35h3ll <yossis@protonmail.com>
2023-02-18 15:18:58 +0200

    the script

---
+++ Get-LogonworkstationsAttributeStatus.ps1
@@ -0,0 +1,17 @@
+﻿# Get-LogonworkstationAttributeStatus, by 1nTh35h311 (comments to yossis@protonmail.com)
+# Essentially, a One-liner to get the latest Change (if ever) in Logonworkstations attribute value (Logon restrictions by user workstations) using Replication property MetaData, without any logging/auditing required
+# No special permissions required, just an authenticated domain user.
+#Requires -Modules ActiveDirectory
+
+param (
+    [cmdletbinding()]
+    [parameter(mandatory=$true)]
+    [string[]]$UserName
+)
+
+$DC = (Get-ADDomainController -Discover).name;
+$UserName | foreach {
+        $account = $_; [string]$Comment=[string]::Empty;
+        $Result = Get-ADReplicationAttributeMetadata $(Get-ADUser $account) -Server $DC | where attributeName -eq "userworkstations";
+        if ($Result) {$Result | select @{n='Username';e={$account}},AttributeValue,@{n='ChangeTime';e={$_.LastOriginatingChangeTime}},@{n='ChangeUSN';e={$_.LastOriginatingChangeUsn}},Version,@{n='Comment';e={if ($_.AttributeValue -eq $null) {$Comment='Reset back to Default (Empty)'};$Comment}}; Clear-Variable Comment} else {"user $account logonworkstations was never set (Empty by default)"}
+    }
