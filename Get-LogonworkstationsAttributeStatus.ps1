a93da2fb583a7fdde3dd14d7b879bacd0ae9bd8f
1nTh35h3ll <yossis@protonmail.com>
2025-05-25 19:18:46 +0300

    v1.1 - fixed minor issue with mistakingly showing 'attribute was reset' when the LastOriginatingChangeTime and WhenCreated are equal)

--- Get-LogonworkstationsAttributeStatus.ps1
+++ Get-LogonworkstationsAttributeStatus.ps1
@@ -1,8 +1,9 @@
 ﻿# Get-LogonworkstationAttributeStatus, by 1nTh35h311 (comments to yossis@protonmail.com)
-# Essentially, a One-liner to get the latest Change (if ever) in Logonworkstations attribute value (Logon restrictions by user workstations) using Replication property MetaData, without any logging/auditing required
+# v1.1 - fixed minor issue with mistakingly showing 'attribute was reset' when the LastOriginatingChangeTime and WhenCreated are equal).
+# Essentially, a One-liner to get the latest Change (if ever) in Logonworkstations attribute value (Logon restrictions by user workstations) using Replication property MetaData, without any logging/auditing required.
 # No special permissions required, just an authenticated domain user.
-#Requires -Modules ActiveDirectory
 
+#Requires -Modules ActiveDirectory
 param (
     [cmdletbinding()]
     [parameter(mandatory=$true)]
@@ -10,8 +11,8 @@ param (
 )
 
 $DC = (Get-ADDomainController -Discover).name;
-$UserName | foreach {
-        $account = $_; [string]$Comment=[string]::Empty;
-        $Result = Get-ADReplicationAttributeMetadata $(Get-ADUser $account) -Server $DC | where attributeName -eq "userworkstations";
-        if ($Result) {$Result | select @{n='Username';e={$account}},AttributeValue,@{n='ChangeTime';e={$_.LastOriginatingChangeTime}},@{n='ChangeUSN';e={$_.LastOriginatingChangeUsn}},Version,@{n='Comment';e={if ($_.AttributeValue -eq $null) {$Comment='Reset back to Default (Cleared)'};$Comment}}; Clear-Variable Comment} else {"user $account logonworkstations was never set (Empty by default)"}
+$UserName -split ',' | foreach {
+        $account = $_; [string]$Comment=[string]::Empty; $UserObj = Get-ADUser $account -Properties WhenCreated;
+        $Result = Get-ADReplicationAttributeMetadata $UserObj -Server $DC | where attributeName -eq "userworkstations";
+        if ($Result) {$Result | select @{n='Username';e={$account}},AttributeValue,@{n='ChangeTime';e={$_.LastOriginatingChangeTime}},@{n='ChangeUSN';e={$_.LastOriginatingChangeUsn}},Version,@{n='Comment';e={if ($_.AttributeValue -eq $null -and $_.LastOriginatingChangeTime -ne $UserObj.WhenCreated) {$Comment='Reset back to Default (Cleared)'};$Comment}}; Clear-Variable Comment} else {"user $account logonworkstations was never set (Empty by default)"}
     }
